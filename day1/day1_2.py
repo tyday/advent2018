f31732597fc7312f7b06f2726a3383da8474fde6
Ty Day <tyrday@gmail.com>
2018-12-19 16:50:46 -0500

    remove some extraneous words

--- day1/day1_2.py
+++ day1/day1_2.py
@@ -10,41 +10,34 @@ It searches for a repeat frequency and returns that number
 """
 import bisect, datetime
 
-def index(a,x):
-    i = bisect.bisect_left(a,x)
-    if i != len(a) and a[i] == x:
-        return i
-    raise ValueError
-
 f_file = open('day1_input.txt','r')
-f_list = []
+frequency_change_list = []
 for change in f_file:
-    f_list.append(int(change.rstrip('\n')))
+    frequency_change_list.append(int(change.rstrip('\n')))
 f_file.close()
+
 frequency_list = []
 current_frequency = 0
 latest_frequency = 0
 frequency_list.append(current_frequency)
+
 frequency_found = False
 loop_iteration = 0 #needed to add this because it was taking so long
 start_time = datetime.datetime.now()
 lap_time = datetime.datetime.now()-start_time
+
 while frequency_found == False:
     loop_start = datetime.datetime.now()
     print(f'Loop: {loop_iteration}, loop time: {lap_time}, total time: {datetime.datetime.now()- start_time}')
-    for change in f_list:
+    for change in frequency_change_list:
         latest_frequency = current_frequency + change
         
         possible_index = bisect.bisect_left(frequency_list, latest_frequency)
         if possible_index != len(frequency_list) and frequency_list[possible_index] == latest_frequency:
             frequency_found = True
             break
-#        if latest_frequency in frequency_list:
-#            frequency_found = True
-#            break
         else:
             current_frequency = latest_frequency
-#            frequency_list.append(current_frequency)
             bisect.insort(frequency_list, current_frequency)
     lap_time = datetime.datetime.now() - loop_start
     loop_iteration += 1
